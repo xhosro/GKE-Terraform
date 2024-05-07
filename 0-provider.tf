57e4e16c1543f612738913359784f1a0a1ff0e7c
xhosro <khosro.hengaam@gmail.com>
2024-05-07 17:46:53 +0200

    terraform fmt & validate

--- 0-provider.tf
+++ 0-provider.tf
@@ -1,29 +1,29 @@
 provider "google" {
-    project = "terraform-422613"
-    region = "eu-west1" # Belgium
+  project = "terraform-422613"
+  region  = "eu-west1" # Belgium
 }
 
 terraform {
-    backend "gcs" {
-      bucket = "bucket-backend-terraform-gcp"
-      prefix = "terraform/state"
-    }
+  backend "gcs" {
+    bucket = "bucket-backend-terraform-gcp"
+    prefix = "terraform/state"
+  }
 
-    required_providers {
-      google = {
-        source = "hashicorp/google"
-        version = "~> 4.0"
-      }
-    }  
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
 }
 
 # in new project we must enable APIs
-resource "google_project_service" "compute" { 
-    service = "compute.googleapis.com"
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
 }
 
-resource "google_project_service" "container"{ 
-    service = "container.googleapis.com"
+resource "google_project_service" "container" {
+  service = "container.googleapis.com"
 }
 
 
--- 3-nodepool.tf
+++ 3-nodepool.tf
@@ -2,72 +2,71 @@
 # The service account maneged by IAM
 # to follow the best practices ,we need to create a dedicated service account
 resource "google_service_account" "kubernetes" {
-     account_id = "kubernetes"
+  account_id = "kubernetes"
 }
 
 # first node is general without tains to be able to run cluster componentssuch as DNS
 resource "google_container_node_pool" "general" {
-    name = "general"
-    cluster = google_container_cluster.primary.id
-    node_count = 1 # 
+  name       = "general"
+  cluster    = google_container_cluster.primary.id
+  node_count = 1 # 
 
-    management {
-        auto_repair = true
-        auto_upgrade = true
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    preemptible  = false      # A boolean that represents whether or not the underlying node VMs are preemptible.
+    machine_type = "e2-small" # large instances and small nodes, since there are a lot of components that need to be deployed such as fluent bit, node exporter,
+
+
+    labels = {
+      role = "general"
     }
 
-    node_config {
-        preemptible = false # A boolean that represents whether or not the underlying node VMs are preemptible.
-        machine_type = "e2-small" # large instances and small nodes, since there are a lot of components that need to be deployed such as fluent bit, node exporter,
-    
-
-        labels = {
-          role = "general"
-        }
-
-        service_account = google_service_account.kubernetes.email
-        oauth_scopes = [
-          "https://www.googleapis.com/auth/cloud-platform"
-        ]
-    }
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
 
 
 # 
 resource "google_container_node_pool" "spot" {
-    name = "spot"
-    cluster = google_container_cluster.primary.id
+  name    = "spot"
+  cluster = google_container_cluster.primary.id
 
-    autoscaling {
-        min_node_count = 1
-        max_node_count = 10
-        target_node_count = 1
+  autoscaling {
+    min_node_count    = 1
+    max_node_count    = 10
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    preemptible  = true # use much cheaper VM for kubernetes nodes # usecase for batch jobs and some data pipelines
+    machine_type = "e2-small"
+
+    labels = {
+      team = "devops"
+      role = "spot"
     }
 
-    management {
-        auto_repair = true
-        auto_upgrade = true
+    # such nodes must have taints to avoid accidental scheduling, your deployment or pod object must tolerate those taints.
+    taint {
+      key    = "spot"
+      effect = "NoSchedule"
+      value  = "true"
     }
 
-    node_config {
-        preemptible = true # use much cheaper VM for kubernetes nodes # usecase for batch jobs and some data pipelines
-        machine_type = "e2-small"
-
-        labels = {
-            team = "devops"
-            role = "spot"
-        }
-        
-        # such nodes must have taints to avoid accidental scheduling, your deployment or pod object must tolerate those taints.
-        taint {
-            key = "spot"
-            effect = "NoSchedule"
-            value = "true"
-        }
-
-        service_account = google_service_account.kubernetes.email
-        oauth_scopes = [
-            "https://www.googleapis.com/auth/cloud-platform"
-        ]
-    }
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 } 
